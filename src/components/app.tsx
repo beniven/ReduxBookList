6941e0c5ef6e6ed10fd834f8c3554346ab52d56a
Ben Niven <beniven@gmail.com>
2018-01-23 11:13:33 -0600

    cleanup

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import BookList from '../containers/book-list';
 
-export default class App extends React.Component<any, any> {
+export default class App extends React.Component {
     render() {
         return (
             <div>
