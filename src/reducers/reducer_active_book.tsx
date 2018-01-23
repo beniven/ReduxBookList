c25797e2c0e7d317068ec65df3955406c6f309d0
Ben Niven <beniven@gmail.com>
2018-01-23 11:45:06 -0600

    added state interface for active book reducer

--- src/reducers/reducer_active_book.tsx
+++ src/reducers/reducer_active_book.tsx
@@ -1,6 +1,6 @@
 // State argument is not application state, only the state
 // this reducter is responsible for
-export default function(state: any = null, action: models.IAction) {
+export default function(state: models.IAppState = null, action: models.IAction) {
     switch (action.type) {
         case 'BOOK_SELECTED':
             return action.payload;
